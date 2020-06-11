d92a76fc026a6a6a404147ee51c032a8fbae032b
Camilo Segura <camilosegurar@gmail.com>
2020-06-11 08:33:29 -0500

    Added AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-camilo-segura"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://7l486yxqnd.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_aKM0KoTFW",
+    APP_CLIENT_ID: "7vi8k73lhntbg4us7c6jv59oek",
+    IDENTITY_POOL_ID: "us-east-1:868acd62-38d9-4159-ba9b-ee8a07c791c7"
+  }
+};
